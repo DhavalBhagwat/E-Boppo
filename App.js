158a305301f93219d37c4db2e47db2e187b98604
Dhaval <dhau2451@gmail.com>
2021-06-20 22:38:24 +0530

    fixed app running issue

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import React from "react";
+import "react-native-gesture-handler";
 import { Provider } from "react-redux";
 import { MainNavigation } from "./src/config/router.js";
 import configureStore from "./src/store/configureStore";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 const store = configureStore();
+export const _navigator = React.createRef();
+export const isReadyRef = React.createRef();
+export let backgroundColor = '#f2f2f2';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <MainNavigation />
-    </Provider>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.app}>
+        <Provider store={store}>
+          <NavigationContainer
+            ref={_navigator}
+            onReady={() => {
+              isReadyRef.current = true;
+            }}>
+            <MainNavigation />
+          </NavigationContainer>
+        </Provider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
-export default App();
+export default App;
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor,
+  },
+});
--- src/config/router.js
+++ src/config/router.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ProductCategory from "../screens/ProductCategory";
 import Product from "../screens/Product";
 import Cart from "../screens/Cart";
@@ -5,40 +6,39 @@ import CheckOut from "../screens/CheckOut";
 import Home from "../screens/Home";
 import { createStackNavigator } from "@react-navigation/stack";
 
-let screens = [
-  {
-    name: "Home",
-    title: null,
-    path: "",
-    screen: Home,
-  },
-  {
-    name: "ProductCategory",
-    title: null,
-    path: "",
-    screen: ProductCategory,
-  },
-  {
-    name: "Product",
-    title: null,
-    path: "",
-    screen: Product,
-  },
-  {
-    name: "Cart",
-    title: null,
-    path: "",
-    screen: Cart,
-  },
-  {
-    name: "CheckOut",
-    title: null,
-    path: "",
-    screen: CheckOut,
-  },
-];
+const Stack = createStackNavigator();
 
-export const MainNavigation = createStackNavigator({
-  screens,
-  home: "Home",
-});
+export const MainNavigation = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{ gestureEnabled: false }}
+    >
+      <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{ title: "Home" }}
+      />
+      <Stack.Screen
+        name="ProductCategory"
+        component={ProductCategory}
+        initialParams={{ user: "me" }}
+      />
+      <Stack.Screen
+        name="Product"
+        component={Product}
+        initialParams={{ user: "me" }}
+      />
+      <Stack.Screen
+        name="Cart"
+        component={Cart}
+        initialParams={{ user: "me" }}
+      />
+      <Stack.Screen
+        name="CheckOut"
+        component={CheckOut}
+        initialParams={{ user: "me" }}
+      />
+    </Stack.Navigator>
+  );
+};
